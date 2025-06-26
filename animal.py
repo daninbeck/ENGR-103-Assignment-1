6a4522894fd8e96ee7c5c5258601d0fd30224557
daninbeck <beckdani@oregonstate.edu>
2025-06-25 21:37:25 -0700

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Danielle Beck
+# GitHub username: daninbeck
+# Date: 6/25/25
+# Description: Asks the user for their favorite animal and then prints out what their favorite animal is
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
